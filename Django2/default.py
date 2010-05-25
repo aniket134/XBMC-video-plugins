0dbf4fb6d4124f2ab13d2618476e2609ddcb6f70
Aniket Mittal <aniket134@gmail.com>
2010-05-25 22:21:49 +0530

    installed python2.4 and pysqlite2

--- Django2/default.py
+++ Django2/default.py
@@ -1,6 +1,7 @@
-import xbmc,xbmcgui,xbmcplugin,sys,os,django,sqlite3_for_xbmc
+import xbmc,xbmcgui,xbmcplugin,sys,os,django
+from pysqlite2 import dbapi2
 
-from xbmc_code import add_links
+#from xbmc_code import add_links
 import init_django
 
 sys.path.append("/home/sh1n0b1/.xbmc/plugins/video/Django2/")
@@ -9,7 +10,11 @@ sys.path.append("/home/sh1n0b1/.xbmc/plugins/video/")
 #Sets the environment variable for Django.
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Django2.settings'
 
-add_links.main()
-init_django.main()
+
+conn = dbapi2.connect('sampledb.db')
+curs = conn.cursor()
+
+#add_links.main()
+#init_django.main()
 
 xbmcplugin.endOfDirectory( handle=int( sys.argv[ 1 ] ), succeeded=True )
