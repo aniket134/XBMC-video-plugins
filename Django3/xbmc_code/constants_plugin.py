b9c6dde4bab1c242a9d2cd94e494ba867461e064
Aniket Mittal <aniket134@gmail.com>
2010-05-27 20:03:08 +0530

    regular commit

--- Django3/xbmc_code/constants_plugin.py
+++ Django3/xbmc_code/constants_plugin.py
@@ -17,8 +17,5 @@ PLUGIN_PATH = '/'
 #: The path to all XBMC video plugins and more importantly it is the path to Django's projects. Used for DJANGO_SETTINGS_MODULE.
 PLUGIN_PATH = PLUGIN_PATH.join(spls)
 
-print(PLUGIN_NAME)
-print(PLUGIN_PATH)
-print(USERNAME)
 #: The environment variable which is used by Django to figure out the for which project it is being used.
 DJANGO_SETTINGS_MODULE = PLUGIN_NAME + '.settings'
