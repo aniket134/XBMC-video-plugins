2eb10f36bd8ec95c0b88d52dde3026854ef7463a
Aniket Mittal <aniket134@gmail.com>
2010-05-27 13:53:51 +0530

    mysql plugin working

--- Django3/default.py
+++ Django3/default.py
@@ -6,21 +6,22 @@ PLUGIN_PATH = '/home/'+ USERNAME +'/.xbmc/plugins/video/'
 
 #Used by django to set the following environment variable
 sys.path.append(PLUGIN_PATH)
+sys.path.append(PLUGIN_PATH + PLUGIN_NAME + '/modules/')
 #Sets the environment variable for Django.
 os.environ['DJANGO_SETTINGS_MODULE'] = PLUGIN_NAME + '.settings'
 
-sys.path.append('usr/lib/python2.6') 
-sys.path.append('/usr/lib/python2.6/plat-linux2') 
-sys.path.append('/usr/lib/python2.6/lib-tk')
-sys.path.append('/usr/lib/python2.6/lib-old') 
-sys.path.append('/usr/lib/python2.6/lib-dynload') 
+#sys.path.append('usr/lib/python2.6') 
+#sys.path.append('/usr/lib/python2.6/plat-linux2') 
+#sys.path.append('/usr/lib/python2.6/lib-tk')
+#sys.path.append('/usr/lib/python2.6/lib-old') 
+#sys.path.append('/usr/lib/python2.6/lib-dynload') 
 sys.path.append('/usr/lib/python2.6/dist-packages') 
-sys.path.append('/usr/lib/python2.6/dist-packages/PIL') 
-sys.path.append('/usr/lib/python2.6/dist-packages/gst-0.10') 
-sys.path.append('/usr/lib/pymodules/python2.6') 
-sys.path.append('/usr/lib/python2.6/dist-packages/gtk-2.0') 
-sys.path.append('/usr/lib/pymodules/python2.6/gtk-2.0') 
-sys.path.append('/usr/local/lib/python2.6/dist-packages')
+#sys.path.append('/usr/lib/python2.6/dist-packages/PIL') 
+#sys.path.append('/usr/lib/python2.6/dist-packages/gst-0.10') 
+#sys.path.append('/usr/lib/pymodules/python2.6') 
+#sys.path.append('/usr/lib/python2.6/dist-packages/gtk-2.0') 
+#sys.path.append('/usr/lib/pymodules/python2.6/gtk-2.0') 
+#sys.path.append('/usr/local/lib/python2.6/dist-packages')
 
 import MySQLdb
 import django
